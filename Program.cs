956448719f7957e1d50718de229c341770ad8639
AndreJeanDias <128902291+AndreJeanDias@users.noreply.github.com>
2023-04-02 11:24:23 -0300

    Musica
    Faça um programa que exiba uma letra de música a sua escolha. Utilize cores em destaque no título e nos refrões.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Tuyo - Narcos Theme Song");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Soy el fuego que arde tu piel");
+Console.WriteLine("Soy el agua que mata tu sed");
+Console.WriteLine("El castillo, la torre, yo soy");
+Console.WriteLine("La espada que guarda el caudal");
+Console.WriteLine();
+
+Console.WriteLine("Tú, el aire que respiro yo");
+Console.WriteLine("Y la luz de la Luna en el mar");
+Console.WriteLine("La garganta que ansio mojar");
+Console.WriteLine("Que temo ahogar de amor");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("¿Y cuáles deseos me vas a dar?");
+Console.WriteLine("Ooh, dices tú: Mi tesoro");
+Console.WriteLine("Basta con mirarlo");
+Console.WriteLine("Y tuyo será");
+Console.ResetColor();
+Console.WriteLine();
